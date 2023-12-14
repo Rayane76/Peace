0ff05f8a6d2c793a7e69d38089c02eeeaf0b8724
Rayane <rayanehiouani.rh@gmail.com>
2023-12-14 22:59:01 +0100

    saasasds

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@
 
 import { gsap } from "gsap"
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import "../styles/styles.css"
 
 
 export default function Home() {
@@ -41,6 +42,10 @@ export default function Home() {
         delay:1.5,
         stagger:1,
         duration:1
+      }).to("#home",{
+        display:"block",
+        opacity:1,
+        duration:0.2
       })
     })
 
@@ -52,6 +57,17 @@ export default function Home() {
     <div id="intro" ref={comp1} style={{height:"100vh",display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column"}}>
     <img id="logo" src="symbol.png" style={{height:"90px",display:"none"}}></img>
     <h1 id="quote" style={{display:"none"}}>Random quote of peace</h1>
+    </div>
+
+    <div id="home" style={{display:"none",opacity:"0"}}>
+       <div className="sideBar">
+        <button style={{all:"unset",width:"100%",height:"50px",textAlign:"center",paddingTop:"8px"}}>Wars</button>
+        <hr></hr>
+        <button style={{all:"unset",width:"100%",height:"50px",textAlign:"center"}}>Hunger</button>
+        <hr></hr>
+        <button style={{all:"unset",width:"100%",height:"50px",textAlign:"center"}}>Natural Disasters</button>
+        <hr></hr>
+       </div>
     </div>
     </>
   )
