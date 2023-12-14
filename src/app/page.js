67d9d5e4292761b07cd98504c72ee8e1b2b54ca0
Rayane <rayanehiouani.rh@gmail.com>
2023-12-14 22:04:33 +0100

    intro

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,58 @@
+'use client'
 
+
+import { gsap } from "gsap"
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 
 export default function Home() {
+
+  const comp1 = useRef(null);
+  useLayoutEffect(()=>{
+    let ctx = gsap.context(()=>{
+      var tl1 = gsap.timeline();
+      tl1.fromTo("#logo",{
+        display:"none",
+        y:250,
+        opacity:0
+      },{
+        opacity:1,
+        y:0,
+        display:"block",
+        duration:"2",
+      }).to("#logo",{
+        display:"none",
+        opacity:"0",
+        duration:0.5,
+        stagger:1,
+      }).
+      fromTo("#quote",{
+        display:"none",
+        opacity:0,
+      },{
+        display:"block",
+        opacity:"1",
+        duration:2,
+        stagger:2
+      })
+      .to("#intro",{
+        display:"none",
+        opacity:"0",
+        delay:1.5,
+        stagger:1,
+        duration:1
+      })
+    })
+
+
+    return () => ctx.revert();
+  },[])
   return (
-   <h1>hello</h1>
+    <>
+    <div id="intro" ref={comp1} style={{height:"100vh",display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column"}}>
+    <img id="logo" src="symbol.png" style={{height:"90px",display:"none"}}></img>
+    <h1 id="quote" style={{display:"none"}}>Random quote of peace</h1>
+    </div>
+    </>
   )
 }
